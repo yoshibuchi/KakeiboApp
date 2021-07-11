1ce3daa4219faab489eccd58d3e60494ff42e957
yoshi0111 <n.yoshibuti.mrs@gmail.com>
2021-07-12 01:45:33 +0900

    incomeモデル_定義

---
+++ Kakeibo/Kakeibo/Models/income.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata;
+
+namespace Kakeibo.Models
+{
+    public class income
+    {
+        public int Id { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime date { get; set; }
+        [ForeignKey("income_category")]
+        public int income_category_id { get; set; }
+
+        public string memo { get; set; }
+
+        public Blob image { get; set; }
+
+    }
+}
---
+++ Kakeibo/Kakeibo/Models/income_category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kakeibo.Models
+{
+    public class income_category
+    {
+        public int Id { get; set; }
+
+        public string category { get; set; }
+    }
+}
